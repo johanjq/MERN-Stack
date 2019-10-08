673a937c964c74d0ad0e7d5c663c79955827d7be
Johan <johan_9_31@hotmail.com>
2019-10-08 19:47:14 +0100

    Backend config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+//  settings
+// name: port, value: 3001
+app.set('port', process.env.PORT || 4000);
+
+//  middlewares
+app.use(cors());
+app.use(express.json());
+
+//  routes
+app.get('/api/users', (req, res) => res.send('Users Routes'))
+app.get('/api/notes', (req, res) => res.send('Notes Routes'))
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ const app = require('./app');
 require('./database');
 
 async function main(){
-    await app.listen(3001);
-    console.log('Server on port 3001');
+    await app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
 }
 
 main();
