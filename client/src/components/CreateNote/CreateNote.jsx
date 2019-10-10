57764821388de7462a729d5d75a2648ae5fa5a9b
Johan <johan_9_31@hotmail.com>
2019-10-10 22:26:06 +0100

    App finished, MERN Stack

--- client/src/components/CreateNote/CreateNote.jsx
+++ client/src/components/CreateNote/CreateNote.jsx
@@ -12,18 +12,45 @@ export class CreateNote extends PureComponent {
       userSelected: "",
       title: "",
       content: "",
-      date: new Date()
+      date: new Date(),
+      editing: false,
+      _id: '',
     };
   }
 
   async componentDidMount() {
     const res = await axios.get("http://localhost:3001/api/users");
-    this.setState({ users: res.data.map(user => user.username) });
-    console.log(this.state.users);
+    this.setState({
+      users: res.data.map(user => user.username),
+      userSelected: res.data[0].username
+    });
+    if (this.props.match.params.id) {
+      const res = await axios.get('http://localhost:3001/api/notes/' + this.props.match.params.id);
+      this.setState({
+        title: res.data.title,
+        content: res.data.content,
+        date: new Date(res.data.date),
+        userSelected: res.data.author,
+        editing: true,
+        _id: this.props.match.params.id
+      });
+    }
   }
 
-  onSubmit = e => {
+  onSubmit = async e => {
     e.preventDefault();
+    const newNote = {
+      title: this.state.title,
+      content: this.state.content,
+      date: this.state.date,
+      author: this.state.userSelected
+    };
+    if (this.state.editing) {
+      await axios.put('http://localhost:3001/api/notes/' + this.state._id, newNote)
+    } else {
+      await axios.post("http://localhost:3001/api/notes", newNote);
+    }
+    window.location.href = "/";
   };
 
   onInputChange = e => {
@@ -41,13 +68,14 @@ export class CreateNote extends PureComponent {
     return (
       <div className="col-md-6 offset-md-3">
         <div className="card card-body">
-          <h4>Create a Note</h4>
+          <h4>{this.props.match.params.id ? 'Edit Note' : 'Create Note'}</h4>
           {/* SELECT USER */}
           <div className="form-group">
             <select
               className="form-control"
               name="userSelected"
               onChange={this.onInputChange}
+              value={this.state.userSelected}
             >
               {this.state.users.map(user => (
                 <option key={user} value={user}>
@@ -63,6 +91,7 @@ export class CreateNote extends PureComponent {
               placeholder="Title"
               name="title"
               onChange={this.onInputChange}
+              value={this.state.title}
               required
             />
           </div>
@@ -72,6 +101,7 @@ export class CreateNote extends PureComponent {
               className="form-control"
               placeholder="content"
               onChange={this.onInputChange}
+              value={this.state.content}
             ></textarea>
           </div>
           <div className="form-group">
--- client/src/components/NotesList/NotesList.jsx
+++ client/src/components/NotesList/NotesList.jsx
@@ -1,16 +1,53 @@
-import React, { PureComponent } from 'react';
-import './styles.css';
+import React, { PureComponent } from "react";
+import axios from "axios";
+import { format } from 'timeago.js';
+import { Link } from 'react-router-dom';
+import "./styles.css";
 
-export class NoteList extends PureComponent{
-    constructor(){
-        super();
-        this.state = {};
-    }
-    render(){
-        return(
-            <div>
-                <h1>Note List</h1>
+export class NoteList extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    };
+  }
+
+  async getNotes() {
+    const res = await axios.get("http://localhost:3001/api/notes");
+    this.setState({ notes: res.data });
+  }
+
+  async componentDidMount() {
+    this.getNotes();
+  }
+
+  deleteNote = async (id) => {
+      await axios.delete('http://localhost:3001/api/notes/' + id);
+      this.getNotes();
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.state.notes.map(note => (
+          <div className="col-md-4 p-2" key={note._id} >
+            <div className="card">
+                <div className="card-header">
+                    <h5>{note.title}</h5>
+                </div>
+              <div className="card-body">
+                <p>{note.content}</p>
+                <p>{note.author}</p>
+                <p>{format(note.date)}</p>
+              </div>
+              <div className="card-footer d-flex justify-content-between">
+                  <button className="btn btn-danger" onClick={() => this.deleteNote(note._id)}>Delete</button>
+                  <Link className="btn btn-secondary" to={"/edit/" + note._id}>Edit</Link>
+              </div>
             </div>
-        )
-    }
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
