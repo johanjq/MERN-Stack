1c3e0644e036e4f59f531258e0965aec9ff9bbc9
Johan <johan_9_31@hotmail.com>
2019-10-10 11:29:11 +0100

    using axios and getting data from server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Navigation } from './components/Navigation';
-import { CreateUser } from './components/CreateUser';
-import { CreateNote } from './components/CreateNote';
-import { NoteList } from './components/NotesList';
-
+import { Navigation } from "./components/Navigation";
+import { CreateUser } from "./components/CreateUser";
+import { CreateNote } from "./components/CreateNote";
+import { NoteList } from "./components/NotesList";
 
 export const App = () => {
-  return(
+  return (
     <Router>
       <Navigation />
-
-      <Route path="/" exact component={NoteList} />
-      <Route path="/edit/:id" component={CreateNote} />
-      <Route path="/create" component={CreateNote} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container p-4">
+        <Route path="/" exact component={NoteList} />
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
-  )
-}
+  );
+};
--- client/src/components/CreateUser/CreateUser.jsx
+++ client/src/components/CreateUser/CreateUser.jsx
@@ -1,16 +1,62 @@
-import React, { PureComponent } from 'react';
-import './styles.css';
+import React, { PureComponent } from "react";
+import axios from "axios";
+import "./styles.css";
 
-export class CreateUser extends PureComponent{
-    constructor(){
-        super();
-        this.state = {};
-    }
-    render(){
-        return(
-            <div>
-                <h1>Create User</h1>
-            </div>
-        )
-    }
+export class CreateUser extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      username: ""
+    };
+  }
+
+  async componentDidMount() {
+    const res = await axios.get("http://localhost:3001/api/users");
+    this.setState({ users: res.data });
+    console.log(this.state.users);
+  }
+
+  onChangeUsername = e => {
+      const { value, name } = e.target;
+      this.setState({
+          [name]: value
+      })
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card card-body">
+            <h3>Create New User</h3>
+            <form>
+              <div className="form-group">
+                <input
+                  placeholder="Username"
+                  type="text"
+                  className="form-control"
+                  onChange={this.onChangeUsername}
+                  name="username"
+                  value={this.state.username}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <ul className="list-grou">
+            {this.state.users.map(user => (
+              <li
+                key={user._id}
+                className="list-group-item list-group-item-action"
+              >
+                {user.username}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
